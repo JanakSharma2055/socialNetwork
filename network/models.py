75a18f5d657542ee6c84f73f918cd8280e1f19b3
Janak Sharma <janak.sharma2075@gmail.com>
2021-04-21 18:44:04 +0545

    all post and profile work done

--- network/models.py
+++ network/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followee")
+
+class Post(models.Model):
+    post = models.CharField(max_length=280)
+    
+    post_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+
+class Likes(models.Model):
+    liked_post=models.ForeignKey(Post,on_delete=models.CASCADE,related_name="liked_post")
+    liked_by=models.ForeignKey(User,on_delete=models.CASCADE,related_name="liker")
--- network/views.py
+++ network/views.py
@@ -4,12 +4,38 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User,Follower,Post,Likes
 
 
 def index(request):
-    return render(request, "network/index.html")
+    if request.method == "POST":
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("index"))
 
+        # Attempt to sign user in
+        content = request.POST["content"]
+        Post.objects.create(post=content, user=request.user)
+        return HttpResponseRedirect(reverse("index"))
+
+    else:
+        all_posts = Post.objects.all().order_by("-post_date")
+        print(all_posts)
+       
+        return render(request, "network/index.html", {
+            "post_data": all_posts
+        })
+
+def profileView(request,user_name):
+    user_details = User.objects.get(username=user_name)
+    post_by_user=user_details.posts.order_by("-post_date").all()
+    print("from profile")
+    print(post_by_user)
+    return render(request,"network/profile.html",
+    {"user":user_details,
+    "posts":post_by_user
+    })
+    
+    
 
 def login_view(request):
     if request.method == "POST":
